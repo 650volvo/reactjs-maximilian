dcbef5a4261e89c1eedfa4cb2f98fe745e3a219b
650volvo <ethan37@tutanota.com>
2022-10-03 10:57:29 -0300

    Add gitignore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
+  const para = document.createElement('p');
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
